27c983f5022095cf477df8f3e07a5acc779e2842
JasonasIgn <ignas.jasonas@gmail.com>
2019-02-25 18:21:32 +0200

    Add apiservice

---
+++ frontend/src/resources/utils/ApiService.js
@@ -0,0 +1,12 @@
+import config from "../../config";
+
+export function request(path, type = "GET", data = null) {
+  const apiPath = `${config.apiUrl}/${path}`;
+  return fetch(apiPath, {
+    method: type,
+    data: data,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  });
+}
